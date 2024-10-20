3b57606de78655e1e2b351e9f8b537cad2d848fe
The Empty <toangaming.203@gmail.com>
2024-10-21 03:27:39 +0700

    Update java.java

--- java.java
+++ java.java
@@ -1,55 +1,75 @@
 import java.io.*;
 
 public class FileSplitJoin {
-    public static String createExt(int fileNo) {
-        if (fileNo < 0) return "";
-        if (fileNo < 10) return ".00" + fileNo;
-        if (fileNo < 100) return ".0" + fileNo;
-        return "." + fileNo;
-    }
-    public static void split(String source, int pSize) throws Exception {
-        FileInputStream fis = new FileInputStream(source);
-        String dest;
-        int fileNo = 1;
-        while (true) {
-            dest = source + createExt(fileNo);
-            FileOutputStream fos = new FileOutputStream(dest);
-            boolean hasMoreData = dataCopy(fis, fos, pSize);
-            fileNo++;
-            fos.close();
-            if (!hasMoreData) break;
-        }
-        fis.close();
-    }
+    
+	public static void main(String[] args) throws Exception {
 
-    public static boolean dataCopy(FileInputStream fis, FileOutputStream fos, long pSize) throws Exception {
-        byte[] buffer = new byte[102400];
-        long remain = pSize;
-        int byteMustRead;
-        while (remain > 0) {
-            byteMustRead = (int) (Math.max(remain, pSize));
-            int byteRead = fis.read(buffer);
-            if (byteRead == -1) return false;
-            fos.write(buffer, 0, byteMustRead);
-            remain -= byteRead;
-        }
+		String path = "E:\\BuildedPJ\\StickerArtBook.apk";
 
-        return true;
-    }
-    public static void join(String destFolder, String partName) throws Exception {
-        String source;
-        String dest = partName.substring(partName.lastIndexOf("."));
-        FileOutputStream fos = new FileOutputStream(dest);
-        int fileNo = 1;
-        while (true) {
-            source = dest + createExt(fileNo);
-            File file = new File(source);
-            if (!file.exists()) break;
-            FileInputStream fis = new FileInputStream(source);
-            dataCopy(fis, fos, file.length());
-            fis.close();
-            fileNo++;
-        }
-        fos.close();
-    }
+		split(path, 90097507);
+		
+//		FileOutputStream fos = new FileOutputStream("E:\\BuildedPJ\\test.1");
+//		fos.close();
+	}
+
+	public static String createExt(int fileNo) {
+
+		if (fileNo < 0)
+			return "";
+
+		if (fileNo < 10)
+			return ".00" + fileNo;
+
+		if (fileNo < 100)
+			return ".0" + fileNo;
+
+		return "." + fileNo;
+	}
+
+	public static void split(String source, int pSize) throws Exception {
+
+		FileInputStream fis = new FileInputStream(source);
+		String dest;
+		int fileNo = 1;
+
+		while (true) {
+			dest = source + createExt(fileNo);
+
+			FileOutputStream fos = new FileOutputStream(dest);
+
+			boolean hasMoreData = dataCopy(fis, fos, pSize);
+
+			fileNo++;
+
+			fos.close();
+
+			if (!hasMoreData)
+				break;
+		}
+
+		fis.close();
+	}
+
+	public static boolean dataCopy(FileInputStream fis, FileOutputStream fos, int pSize) throws Exception {
+		byte[] buffer = new byte[102400];
+		int remain = pSize;
+		int readByte;
+		int reqByte;
+
+		while (remain > 0) {
+
+			reqByte = (remain < buffer.length) ? remain : buffer.length;
+			
+			readByte = fis.read(buffer, 0, reqByte);
+
+			if (readByte == -1)
+				return false;
+
+			fos.write(buffer, 0, readByte);
+
+			remain -= readByte;
+		}
+
+		return true;
+	}
 }
